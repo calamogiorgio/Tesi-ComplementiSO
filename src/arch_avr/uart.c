1baeb51ef780613347662f78c1a7e6fd0f936184
Giorgio <calamo.2131075@studenti.uniroma1.it>
2026-05-27 19:19:08 +0200

    Refactoring completo del driver UART: sostituito il meccanismo di polling bloccante con una gestione interamente asincrona basata su interrupt (RX e UDRE) e ring buffer circolari per la trasmissione e ricezione dei dati

--- src/arch_avr/uart.c
+++ src/arch_avr/uart.c
@@ -1,4 +1,5 @@
 #include "uart.h"
+#include "tcb.h"
 // ********************************************************************************
 // Includes
 // ********************************************************************************
@@ -11,16 +12,41 @@
 // ********************************************************************************
 #define BAUD 19600
 #define MYUBRR F_CPU/16/BAUD-1
-// ********************************************************************************
-// Function Prototypes
-// ********************************************************************************
-void usart_init(uint16_t ubrr);
-char usart_getchar( void );
-void usart_putchar( char data );
-void usart_pstr (char *s);
-unsigned char usart_kbhit(void);
-int usart_putchar_printf(char var, FILE *stream);
 
+
+// ********************************************************************************
+// Ring Buffer Static Instances
+// ********************************************************************************
+static RingBuffer rx_buffer;
+static RingBuffer tx_buffer;
+
+// ********************************************************************************
+// RingBuffer Related
+// ********************************************************************************
+
+
+void RingBuffer_init(RingBuffer *rb) {
+    rb->head = 0;
+    rb->tail = 0;
+}
+
+uint8_t RingBuffer_push(RingBuffer *rb, uint8_t data) {
+    // Check if the buffer is full using an optimized bitwise AND formula
+    if (((rb->head + 1) & (UART_BUFFER_SIZE - 1)) == rb->tail) return ERROR;
+    
+    rb->buffer[rb->head] = data;
+    rb->head = (rb->head + 1) & (UART_BUFFER_SIZE - 1);
+    return OK;
+}
+
+uint8_t RingBuffer_pop(RingBuffer *rb, uint8_t *data) {
+    // Check if the buffer is empty
+    if (rb->head == rb->tail) return ERROR;
+
+    *data = rb->buffer[rb->tail];
+    rb->tail = (rb->tail + 1) & (UART_BUFFER_SIZE - 1);
+    return OK;
+}
 
 static FILE mystdout = FDEV_SETUP_STREAM(usart_putchar_printf, NULL, _FDEV_SETUP_WRITE);
 
@@ -32,27 +58,53 @@ void usart_init( uint16_t ubrr) {
     UBRR0H = (uint8_t)(ubrr>>8);
     UBRR0L = (uint8_t)ubrr;
 
-    UCSR0C = _BV(UCSZ01) | _BV(UCSZ00); /* 8-bit data */ 
-    UCSR0B = _BV(RXEN0) | _BV(TXEN0) | _BV(RXCIE0);   /* Enable RX and TX */  
+    UCSR0C = _BV(UCSZ01) | _BV(UCSZ00); // 8-bit data
+    UCSR0B = _BV(RXEN0) | _BV(TXEN0) | _BV(RXCIE0);   // Enable RX and TX
+
+    // Initialize software ring buffers for transmission and reception
+    RingBuffer_init(&rx_buffer);
+    RingBuffer_init(&tx_buffer);
 }
+// Transmit a character using the ring buffer
 void usart_putchar(char data) {
-    // Wait for empty transmit buffer
-    while ( !(UCSR0A & (_BV(UDRE0))) );
-    // Start transmission
-    UDR0 = data; 
+    uint8_t ret = RingBuffer_push(&tx_buffer, data);
+    
+    // If the character was buffered successfully, enable the Data Register Empty Interrupt
+    if (ret == OK) UCSR0B |= _BV(UDRIE0);
 }
+
+// Receive a character from the ring buffer
 char usart_getchar(void) {
-    // Wait for incoming data
-    while ( !(UCSR0A & (_BV(RXC0))) );
-    // Return the data
-    return UDR0;
+    char data_extracted = 0;
+    uint8_t ret = RingBuffer_pop(&rx_buffer, (uint8_t *)&data_extracted);
+    
+    // Return the extracted character if available, otherwise return 0
+    if (ret == OK) return data_extracted;
+    else return 0;
 }
+
+// Interrupt Service Routine for USART0 Data Register Empty
+ISR(USART0_UDRE_vect) {
+    char data = 0;
+    uint8_t ret = RingBuffer_pop(&tx_buffer, (uint8_t *)&data);
+    
+    // If there is data in the buffer, send it; otherwise, disable this interrupt
+    if (ret == OK) UDR0 = data;
+    else UCSR0B &= ~(_BV(UDRIE0));
+}
+
+// Interrupt Service Routine for USART0 Receive Complete
+ISR(USART0_RX_vect) {
+    // Read the received byte from the hardware data register
+    uint8_t data = UDR0;
+    
+    // Push the byte into the software receive buffer
+    RingBuffer_push(&rx_buffer, data);
+}
+
 unsigned char usart_kbhit(void) {
-    //return nonzero if char waiting polled version
-    unsigned char b;
-    b=0;
-    if(UCSR0A & (1<<RXC0)) b=1;
-    return b;
+    if (rx_buffer.head == rx_buffer.tail) return 0;
+    else return 1;
 }
 void usart_pstr(char *s) {
     // loop through entire string
